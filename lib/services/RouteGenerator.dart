db8c0bb5924dd09737674f2b923c80f7d83d4dc3
SerpenTaki <gabrielecorubolo@gmail.com>
2025-09-26 11:32:11 +0200

    Aggiustato secureStorage
    - Aggiunta classe SecureStorage per gestire i dati che vengono salvati nel dispositivo (testare IOS)
    - Inizio pagina del Wallet
    - Fix pagina di autenticazione

--- lib/services/RouteGenerator.dart
+++ lib/services/RouteGenerator.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:key_wallet_app/models/wallet.dart'; // Importa il modello Wallet
 import 'package:key_wallet_app/screens/_landingPage.dart';
 import 'package:key_wallet_app/screens/AuthPage.dart';
+import 'package:key_wallet_app/screens/_walletPage.dart'; // Assicurati che WalletPage sia importata
 
 //Per mandare dati dinamici
 class RouteGenerator {
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
 
@@ -12,6 +15,13 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (context) => LandingPage());
       case '/AuthPage':
         return MaterialPageRoute(builder: (context) => AuthPage());
+      case '/WalletPage':
+        if (args is Wallet) {
+          return MaterialPageRoute(
+            builder: (context) => WalletPage(wallet: args),
+          );
+        }
+        return _errorRoute();
       default:
         return _errorRoute();
     }
@@ -19,7 +29,10 @@ class RouteGenerator {
 
   static Route<dynamic> _errorRoute() {
     return MaterialPageRoute(
-      builder: (_) => Scaffold(body: Center(child: Text('Error'))),
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: Text('Errore')),
+        body: Center(child: Text('Pagina non trovata o errore negli argomenti.')),
+      ),
     );
   }
 }
