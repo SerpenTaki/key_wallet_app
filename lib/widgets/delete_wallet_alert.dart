5a10f42a65d697d2c1b8215cab378981c8aa53d7
SerpenTaki <gabrielecorubolo@gmail.com>
2025-09-29 11:23:44 +0200

    pulizia codice e sistemato qualche widget

---
+++ lib/widgets/delete_wallet_alert.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DeleteWalletAlert extends StatelessWidget {
+  const DeleteWalletAlert({super.key, required this.walletName, required this.dialogContext});
+
+  final String walletName;
+  final BuildContext dialogContext;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Conferma Eliminazione'),
+      content: Text(
+        'Sei sicuro di voler eliminare il wallet "$walletName"? Questa azione è irreversibile e la chiave privata verrà rimossa da questo dispositivo.',
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Annulla'),
+          onPressed: () =>
+              Navigator.of(dialogContext).pop(false),
+        ),
+        TextButton(
+          child: const Text(
+            'Elimina',
+            style: TextStyle(color: Colors.red),
+          ),
+          onPressed: () =>
+              Navigator.of(dialogContext).pop(true),
+        ),
+      ],
+    );
+  }
+}
