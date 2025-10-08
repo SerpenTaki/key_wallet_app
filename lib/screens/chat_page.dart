5d61858de6f8da70b4f7ff6b8e442a5bf4577cbf
SerpenTaki <gabrielecorubolo@gmail.com>
2025-10-08 17:11:24 +0200

    class separation

--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:key_wallet_app/services/chat/chat_service.dart';
 import 'package:key_wallet_app/widgets/chatWidgets/build_message_list.dart';
+import 'package:key_wallet_app/widgets/chatWidgets/build_user_input.dart';
 
 class ChatPage extends StatefulWidget {
   final Map<String, dynamic> senderWallet;
@@ -17,20 +17,6 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final TextEditingController _messageController = TextEditingController();
-  final ChatService _chatService = ChatService();
-
-  void sendMessage() async {
-    if (_messageController.text.isNotEmpty) {
-      await _chatService.sendMessage(
-        widget.receiverWallet['id'],
-        widget.senderWallet['id'],
-        _messageController.text,
-      );
-    }
-
-    _messageController.clear();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,44 +39,8 @@ class _ChatPageState extends State<ChatPage> {
             ),
           ),
           //mostra il campo per inviare messaggi
-          _buildUserInput(),
+          BuildUserInput(receiverWalletId: widget.receiverWallet['id'], senderWalletId: widget.senderWallet['id'],),
           const SizedBox(height: 10),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildUserInput(){
-    return Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              controller: _messageController,
-              decoration: InputDecoration(
-                hintText: 'Scrivi un messaggio...',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: const BorderRadius.all(Radius.circular(10)),
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.secondary,
-              shape: BoxShape.circle,
-            ),
-            margin: const EdgeInsets.only(left: 10),
-            child: IconButton(
-              icon: const Icon(Icons.send, color: Colors.white),
-              onPressed: sendMessage,
-            ),
-          ),
         ],
       ),
     );
