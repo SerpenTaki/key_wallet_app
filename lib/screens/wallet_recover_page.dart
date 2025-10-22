dea6ca8e8d9ea5337e8ccbb3797c94a8b542d1d0
SerpenTaki <gabrielecorubolo@gmail.com>
2025-10-22 16:42:22 +0200

    frontend pagina recupero

--- lib/screens/wallet_recover_page.dart
+++ lib/screens/wallet_recover_page.dart
@@ -5,6 +5,52 @@ class WalletRecoverPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recupera Wallet', style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 20,
+          children: <Widget>[
+            const Text(
+              "Attenzione questa pagina è riservata al recupero di un wallet nel caso di eliminazione della chiave privata, o per aumentare gli accessi ad esso",
+              style: TextStyle(
+                fontSize: 15
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  label: const Text('Chiave privata'),
+                  fillColor: Colors.grey[200],
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Theme.of(context).colorScheme.primary,),
+                  ),
+                  hintText: 'Inserisci la chiave privata',
+                ),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(390, 50),
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+                ),
+                child: const Text("Recupera Wallet", style: TextStyle(fontWeight: FontWeight.bold),)
+            ),
+          ]
+        ),
+      ),
+    );
   }
 }
