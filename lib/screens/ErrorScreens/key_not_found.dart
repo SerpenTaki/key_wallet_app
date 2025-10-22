051cd5ca6f7d3982ca06004c5cf1036454f1d530
SerpenTaki <gabrielecorubolo@gmail.com>
2025-10-22 16:21:43 +0200

    Aggiornata la pagina di errore

--- lib/screens/ErrorScreens/key_not_found.dart
+++ lib/screens/ErrorScreens/key_not_found.dart
@@ -13,8 +13,26 @@ class KeyNotFound extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
+            Image.asset("images/logo.png", width: 100, height: 100),
             const Text('Chiave privata non trovata!', textAlign: TextAlign.center, style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-            const Text("Riprova da un altro dispositivo", textAlign: TextAlign.center,),
+            const Text("Riprova da un altro dispositivo o recupera il tuo wallet", textAlign: TextAlign.center, style: TextStyle(fontSize: 15),),
+            const SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
+                  onPressed: (){
+
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(390, 50),
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  child: const Text("Recupera Wallet", style: TextStyle(fontWeight: FontWeight.bold),)
+            ),
+            const SizedBox(
+              height: 2,
+            ),
             ElevatedButton(
               onPressed: () {
                 if (Navigator.canPop(context)) {
@@ -22,10 +40,11 @@ class KeyNotFound extends StatelessWidget {
                 }
               },
               style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary,
+                minimumSize: const Size(390, 50),
+                backgroundColor: Theme.of(context).colorScheme.secondary,
                 foregroundColor: Theme.of(context).colorScheme.inversePrimary,
               ),
-              child: const Text("Torna indietro"),
+              child: const Text("Torna indietro", style: TextStyle(fontWeight: FontWeight.bold),),
             ),
           ],
         ),
